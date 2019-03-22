88f93cfbf3898502cd6dfb5228ea5d27fc05776e
esveas <mr.sergeychik@gmail.com>
2019-03-22 05:36:19 +0200

    delete jquery

--- js/script.js
+++ js/script.js
@@ -25,26 +25,21 @@ function stepTime() {
 function stepSecond() {
   let rotateSecond = second * 6;
   secondHTML.style.transform = "rotate(" + rotateSecond + "deg)";
-  secondHTML.style.webkitTransform = "rotate(" + rotateSecond + "deg)";
   let eachSecond = setInterval(function() {
     rotateSecond += 6;
     secondHTML.style.transform = "rotate(" + rotateSecond + "deg)";
-    secondHTML.style.webkitTransform = "rotate(" + rotateSecond + "deg)";
   }, 1000);
 }
 
 function stepMinute() {
   let rotateMinute = minute * 6;
   minuteHTML.style.transform = "rotate(" + rotateMinute + "deg)";
-  minuteHTML.style.webkitTransform = "rotate(" + rotateMinute + "deg)";
   setTimeout(function() {
     rotateMinute += 6;
     minuteHTML.style.transform = "rotate(" + rotateMinute + "deg)";
-    minuteHTML.style.webkitTransform = "rotate(" + rotateMinute + "deg)";
     let eachMinute = setInterval(function() {
       rotateMinute += 6;
       minuteHTML.style.transform = "rotate(" + rotateMinute + "deg)";
-      minuteHTML.style.webkitTransform = "rotate(" + rotateMinute + "deg)";
     }, 60000);
   }, (60 - second) * 1000);
 }
@@ -55,15 +50,12 @@ function stepHour() {
   }
   let rotateHour = hour * 30;
   hourHTML.style.transform = "rotate(" + rotateHour + "deg)";
-  hourHTML.style.webkitTransform = "rotate(" + rotateHour + "deg)";
   setTimeout(function() {
     rotateHour += 30;
     hourHTML.style.transform = "rotate(" + rotateHour + "deg)";
-    hourHTML.style.webkitTransform = "rotate(" + rotateHour + "deg)";
     let eachHour = setInterval(function() {
       rotateHour += 30;
       hourHTML.style.transform = "rotate(" + rotateHour + "deg)";
-      hourHTML.style.webkitTransform = "rotate(" + rotateHour + "deg)";
     }, 3600000);
   }, (60 - minute) * 60000);
 }
